13c9942aa7fafcb64475d0c6a4f47c7efd226c39
EbunAdee <estheridonije@gmail.com>
2024-01-24 07:01:33 -0800

    configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,14 @@
 # Install flask v2.1.0 from pip3
-package { 'flask':
+
+# Define package resource for Flask
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+}
+
+# Specify version for Werkzeug
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['Flask'],
 }
